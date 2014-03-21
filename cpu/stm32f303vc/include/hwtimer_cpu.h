5087ba08890d7e8882aba4491772034852be7961
Hauke Petersen <hauke.petersen@fu-berlin.de>
2014-03-21 11:02:21 +0100

    CLEANUP-1 removal of temporal and moved files

--- cpu/stm32f303vc/include/hwtimer_cpu.h
+++
@@ -1,9 +0,0 @@
-
-#ifndef HWTIMER_CPU_H_
-#define HWTIMER_CPU_H_
-
-#define HWTIMER_MAXTIMERS   4               ///< the cpu implementation supports 4 HW timers
-#define HWTIMER_SPEED       1000000         ///< the HW timer runs with 1MHz
-#define HWTIMER_MAXTICKS    (0xFFFFFFFF)    ///< 32-bit timer
-
-#endif /* HWTIMER_CPU_H_ */
